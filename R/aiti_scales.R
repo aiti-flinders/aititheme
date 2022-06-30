dfd6b6051799e75ca278686e80b507a62be3d9c0
Hamish Gamble <gamb0043@flinders.edu.au>
2022-06-30 14:16:58 +0930

    Implement scales for colour/fill

--- R/aiti_scales.R
+++ R/aiti_scales.R
@@ -1,45 +1,49 @@
-#' Set manual colours
+#' Colour scale with AITI palettes. 
+#' 
+#' @param palette 
 #'
-#' @param n number of colours
-#' @param reverse (logical) whether to reverse the order of the colours 
-#' @param discrete (logical) 
-#' @param faded (logical) NYI
-#' @param palette NYI
-#' @param ... other elements passed to ggplot2::scales
+#' @param discrete 
+#' @param reverse 
+#' @param ... 
 #'
-#'
-#' @name aiti_scale
-#'
-#' @rdname aiti_scale
-#' @import ggplot2
-#' @export aiti_colour_manual
-aiti_colour_manual <- function(n = 0,
-                               reverse = FALSE,
-                               discrete = TRUE,
-                               faded = FALSE,
-                               palette = "full", ...) {
+#' @export scale_colour_aiti
+scale_colour_aiti <- function(palette = "main", 
+                              discrete = TRUE,
+                              reverse = FALSE,
+                              ...) {
+  
+  pal <- aiti_pal(palette = palette, reverse = reverse)
+  
 
-  if(discrete) {
-    return(
-      ggplot2::scale_colour_manual(...,
-                                   values = aititheme::aiti_pal(n = n,
-                                                                reverse = reverse,
-                                                                faded = faded))
-    )
+  if (discrete) {
+    discrete_scale("colour", paste0("aiti_", palette), palette = pal, ...)
+  } else {
+    scale_colour_gradientn(colours = pal(256), ...)
   }
 
-  if (!discrete) {
-    pal <- aiti_palette(palette = palette, reverse = reverse)
-    return(ggplot2::scale_colour_gradientn(colours = pal(256), ...))
-  }
+}
 
-  aiti_palette <- function(palette = "full", reverse = FALSE, ...) {
-
-    pal <- aititheme::aiti_palette_set[[palette]]
-
-    if (reverse) pal <- rev(pal)
-
-    grDevices::colorRampPalette(pal, ...)
+#' Fill scale with AITI palettes. 
+#'
+#' @param palette 
+#' @param discrete 
+#' @param reverse 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+scale_fill_aiti <- function(palette = "main", 
+                            discrete = TRUE,
+                            reverse = FALSE,
+                            ...) {
+  pal <- aiti_pal(palette = palette, reverse = reverse)
+  
+  if (discrete) {
+    discrete_scale("fill", paste0("aiti_", palette), palette = pal,)
+  } else {
+    scale_fill_gradient_n(colours = pal(256), ...)
   }
 }
 
