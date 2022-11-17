de63e971642ba05cb216c029c951ee7f42eb2e6d
Hamish Gamble <gamb0043@flinders.edu.au>
2022-11-17 13:07:05 +1030

    Allow markdown in aiti plots

--- R/theme_aiti.R
+++ R/theme_aiti.R
@@ -11,69 +11,77 @@
 #' @param base_family 
 #' @param title_family Plot title font family
 #' @param markdown Include \code{ggtext::element_markdown}
+#' @param flipped 
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #' 
-#' @importFrom ggplot2 element_line element_rect element_text element_blank rel
+#' @importFrom ggplot2 element_line element_rect element_text element_blank rel margin unit
+#' @importFrom ggtext element_markdown
 theme_aiti <- function(base_size = 12,
                        colour = "blue",
                        base_family = "sans",
                        title_family = "mono",
+                       markdown = FALSE,
                        flipped = FALSE) {
   
   bg_colour <- aititheme::theme_data$aiti$bg[colour]
   
   thm <- theme_foundation(base_size = base_size, base_family = base_family) +
-    theme(line = element_line(linetype = 1, colour = "black", size = 0.25),
-          rect = element_rect(fill = bg_colour, 
-                              linetype = 0,
-                              colour = NA),
-          text = element_text(colour = "black", lineheight = 0.9),
-          axis.title = element_text(size = rel(1)), 
-          axis.title.x = element_text(margin = margin(t = 6),
-                                      vjust = 1),
-          axis.title.y = element_text(angle = 90,
-                                      vjust = 1),
-          axis.line = element_line(size = 0.35, colour = "black"),
-          axis.line.y = element_blank(),
-          axis.ticks.y = element_blank(),
-          axis.line.x = NULL,
-          axis.text = element_text(face = "bold", size = rel(1)),
-          axis.text.x = element_text(colour = NULL),
-          axis.text.y = element_text(colour = NULL),
-          axis.ticks = element_line(colour = "black"),
-          axis.ticks.length = unit(6, "pt"),
-          axis.ticks.length.x = NULL,
-          axis.ticks.length.x.top = NULL,
-          axis.ticks.length.x.bottom = NULL,
-          axis.ticks.length.y = NULL,
-          axis.ticks.length.y.left = NULL,
-          axis.ticks.length.y.right = NULL,
-          legend.background = element_rect(),
-          legend.position = "bottom",
-          legend.direction = "horizontal",
-          legend.justification = "center",
-          legend.title = element_blank(), 
-          legend.box = "vertical",
-          panel.grid = element_line(colour = "black", size = 0.15),
-          panel.grid.major.x = element_blank(),
-          panel.border = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.spacing = unit(1, "lines"),
-          panel.spacing.x = NULL,
-          panel.spacing.y = NULL,
-          plot.title = element_text(face = "bold", hjust = 0),
-          plot.margin = unit(c(1,1,1,1), "lines"),
-          strip.background = element_rect()
+    ggplot2::theme(line = element_line(linetype = 1, colour = "black", size = 0.25),
+                   rect = element_rect(fill = bg_colour, 
+                                       linetype = 0,
+                                       colour = NA),
+                   text = element_text(colour = "black", lineheight = 0.9),
+                   axis.title = element_text(size = rel(1)), 
+                   axis.title.x = element_text(margin = margin(t = 6),
+                                               vjust = 1),
+                   axis.title.y = element_text(angle = 90,
+                                               vjust = 1),
+                   axis.line = element_line(size = 0.35, colour = "black"),
+                   axis.line.y = element_blank(),
+                   axis.ticks.y = element_blank(),
+                   axis.line.x = NULL,
+                   axis.text = element_text(face = "bold", size = rel(1)),
+                   axis.text.x = element_text(colour = NULL),
+                   axis.text.y = element_text(colour = NULL),
+                   axis.ticks = element_line(colour = "black"),
+                   axis.ticks.length = unit(6, "pt"),
+                   axis.ticks.length.x = NULL,
+                   axis.ticks.length.x.top = NULL,
+                   axis.ticks.length.x.bottom = NULL,
+                   axis.ticks.length.y = NULL,
+                   axis.ticks.length.y.left = NULL,
+                   axis.ticks.length.y.right = NULL,
+                   legend.background = element_rect(),
+                   legend.position = "bottom",
+                   legend.direction = "horizontal",
+                   legend.justification = "center",
+                   legend.title = element_blank(), 
+                   legend.box = "vertical",
+                   panel.grid = element_line(colour = "black", size = 0.15),
+                   panel.grid.major.x = element_blank(),
+                   panel.border = element_blank(),
+                   panel.grid.minor = element_blank(),
+                   plot.title = element_text(face = "bold", hjust = 0),
+                   plot.margin = unit(c(1,1,1,1), "lines"),
+                   strip.background = element_rect()
     )
   
   if (flipped) {
     thm <- thm %+replace% 
-      theme(panel.grid.major.x = element_line(),
-            panel.grid.major.y = element_blank())
+      ggplot2::theme(panel.grid.major.x = element_line(),
+                     panel.grid.major.y = element_blank())
+  }
+  
+  if (markdown) {
+    thm <- thm %+replace%
+      ggplot2::theme(plot.title = ggtext::element_markdown(),
+                     plot.subtitle = ggtext::element_markdown(),
+                     axis.title.x = ggtext::element_markdown(),
+                     axis.title.y = ggtext::element_markdown())
   }
   
   thm
