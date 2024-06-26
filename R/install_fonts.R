e00207b35bafcd2bfd22a8bb026a8a7ae0fc72c5
Hamish Gamble <hamish.gamble@flinders.edu.au>
2024-06-26 16:01:20 +0930

    Adds function to install fonts using sysfonts

--- R/install_fonts.R
+++ R/install_fonts.R
@@ -1,8 +1,16 @@
-install_aiti_fonts <- function() {
+install_aiti_fonts <- function(theme = c("old", "new")) {
   requireNamespace("sysfonts", quietly = TRUE)
   
-  cli::cli_progress_step("Installing Space Mono font from Google Fonts")
-  sysfonts::font_add_google("Space Mono", "Space Mono")
-  cli::cli_progress_step("Successfully installed Space Mono font")
+  theme <- match.arg(theme)
+  
+  font <- switch(
+    theme,
+    old = "Roboto",
+    new = "Space Mono"
+  )
+  
+  cli::cli_progress_step("Installing {font} font from Google Fonts")
+  sysfonts::font_add_google(font, font)
+  cli::cli_progress_step("Successfully installed {font} font")
   
 }
